b3c3f3c9e40435449de87cdeade0282382394479
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2026-04-02 21:15:45 +0300

    move all hello worlds to folder

--- hello_world/HelloWorld.cs
+++ hello_world/HelloWorld.cs
@@ -1,7 +1,5 @@
-using System;
-
 public class HelloWorld {
   public static void Main(string[] args) {
-    Console.WriteLine("Hello, World!");
+    System.Console.WriteLine("Hello, World!");
   }
 }
